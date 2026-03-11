3b6afa364528f645bfb0112371053c42344bc5b3
RenoMelon <reno.melon@student.pxl.be>
2026-03-11 19:45:15 +0100

    codecrafters submit [skip ci]

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -40,7 +40,7 @@ public class Commands {
         return Optional.empty();
     }
 
-    //test
+    //test 2
     public static List<String> inputTokenizer(String input){
         List<String> args = new ArrayList<>();
         StringBuilder currentArg = new StringBuilder();
