c78df308fe546719abe3937dffb3631f58f12811
RenoMelon <reno.melon@student.pxl.be>
2026-03-14 23:56:30 +0100

    codecrafters submit [skip ci]

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -176,7 +176,7 @@ class Pwd implements Command{
         System.out.println(Commands.currentWorkingDir.toAbsolutePath());
     }
 }
-//d
+//
 class Cd implements Command{
 
     public void execute(String[] args) {
