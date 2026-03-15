daa77eb2d19babbc0f60dc9714563d937e7af148
RenoMelon <reno.melon@student.pxl.be>
2026-03-15 11:16:54 +0100

    codecrafters submit [skip ci]

--- src/main/java/ShellCompleter.java
+++ src/main/java/ShellCompleter.java
@@ -1,8 +1,3 @@
-import org.jline.reader.Candidate;
-import org.jline.reader.Completer;
-import org.jline.reader.LineReader;
-import org.jline.reader.ParsedLine;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,31 +5,27 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class ShellCompleter {
-    public static List<String> getMatches(String prefix){
-        Set<String> matches = new TreeSet<>();
+    public static List<String> getMatches(String prefix) {
+        Set<String> matches = new TreeSet<>(); // TreeSet = auto gesorteerd
 
         Commands.commands.keySet().stream()
                 .filter(name -> name.startsWith(prefix))
                 .forEach(matches::add);
 
-        // External executables completion
         String pathEnv = System.getenv("PATH");
-        if(pathEnv == null) return new ArrayList<>(matches);
+        if (pathEnv == null) return new ArrayList<>(matches);
 
-        for(String dir : pathEnv.split(File.pathSeparator)) {
+        for (String dir : pathEnv.split(File.pathSeparator)) {
             File folder = new File(dir);
-            if(!folder.exists() || !folder.isDirectory()) continue;
-
+            if (!folder.exists() || !folder.isDirectory()) continue;
             File[] files = folder.listFiles();
-            if(files == null) continue;
-
-            for(File file : files){
-                if(file.getName().startsWith(prefix) && file.canExecute()){
+            if (files == null) continue;
+            for (File file : files) {
+                if (file.getName().startsWith(prefix) && file.canExecute())
                     matches.add(file.getName());
-                }
             }
         }
         return new ArrayList<>(matches);
     }
-
 }
+
